cc791cfcb84f195f178e0c49b07b2976fa199f51
= <=>
2023-10-25 06:44:05 -0600

    lint

--- src/app/(routes)/friends/page.tsx
+++ src/app/(routes)/friends/page.tsx
@@ -1,12 +1,21 @@
 import prisma from "@/lib/db";
 import { PageTitle } from "../../pageTitle";
 
-const getFriends = async () => {
-  return prisma.user.findMany();
+type Friend = {
+  id: string;
+  firstName: string | null;
+  lastName: string | null;
+};
+
+const getFriends = async (): Promise<Friend[]> => {
+  return prisma.user.findMany({
+    select: { id: true, firstName: true, lastName: true },
+  });
 };
 
 const FriendsPage = async () => {
   const friends = await getFriends();
+
   return (
     <div>
       <PageTitle title="Friends" />
